d12e8235d9111a87b10159fd30237e6d9aca0ad0
Takla Gerguis <taklagerguis@gmail.com>
2020-02-08 23:41:45 +0200

    Delete AirportServiceImpl.java

--- FlightBookingSystem/src/main/java/com/brightskies/FlightBookingSystem/service/AirportServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.brightskies.FlightBookingSystem.service;
-
-import com.brightskies.FlightBookingSystem.DAO.AirportRepository;
-
-public class AirportServiceImpl implements AirportRepository {
-
-}
